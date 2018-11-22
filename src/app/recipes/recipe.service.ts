0fdbdae5da5e1653f2fe3803c318ba4befec3490
JoshMR94 <joshuamarram@gmail.com>
2018-11-22 13:51:18 +0000

    See last commit changes for info

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,39 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
-import { EventEmitter } from "@angular/core";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService {
 
     recipeSelected = new EventEmitter<Recipe>();
     
     private recipes: Recipe[] = [
-        new Recipe('Test Recipe', 'Test Description', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg'),
-        new Recipe('Another Test Recipe', 'Another Test Description', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg')
+        new Recipe('Test Recipe',
+        'Test Description', 
+        'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg',
+        [
+            new Ingredient("Cheese", 2),
+            new Ingredient("Meat", 3)
+        ]
+        ),
+        new Recipe('Another Test Recipe',
+        'Another Test Description', 
+        'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg',
+        [
+            new Ingredient("Bread", 4),
+            new Ingredient("Meat", 5)
+        ]
+        )
     ];
+
+    constructor(private shoppingListService: ShoppingListService) {}
 
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.shoppingListService.addIngredients(ingredients);
     }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,35 @@
+import { Ingredient } from "../shared/ingredient.model";
+import { EventEmitter } from "@angular/core";
+
 export class ShoppingListService {
-    
+
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
+
+    private ingredients: Ingredient[] = [
+        new Ingredient("Apples", 5),
+        new Ingredient("Meat", 3),
+        new Ingredient("Pears", 4),
+    ];
+
+    getIngredients() {
+        return this.ingredients.slice();
+    }
+
+    addIngredient(ingredient: Ingredient) {
+        this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice())
+    }
+
+    addIngredients(ingredients: Ingredient[]) {
+        /*
+        for (let ingredient of ingredients) {
+            this.addIngredient(ingredient);
+        }
+        */
+       //... spread operator. Permits to propagate elements that can be iterable, separating their elements.
+       //Allows us to basically turn an array of elements in a list of elements
+       //In this case, push inserts every one of the elements in ingredients array to other array
+        this.ingredients.push(...ingredients);
+        this.ingredientsChanged.emit(this.ingredients.slice());
+    }
 }
